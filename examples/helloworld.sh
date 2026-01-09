6000c4a38f20a6066f56eabcf3531392a9379af9
youngtaekiim <ytkim0209@gmail.com>
2026-01-09 16:29:29 +0900

    fix hardcoding IP address

--- examples/helloworld.sh
+++ examples/helloworld.sh
@@ -7,6 +7,6 @@ BODY=$(< ./resources/helloworld_no_condition.yaml)
 
 #URL="10.0.0.30:8080/api/v1/yaml"
 
-curl -X POST 'http://10.232.122.114:47099/api/artifact' \
+curl -X POST 'http://127.0.0.1:47099/api/artifact' \
 --header 'Content-Type: text/plain' \
 --data "${BODY}"
